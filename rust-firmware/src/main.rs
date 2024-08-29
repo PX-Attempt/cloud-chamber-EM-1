0c81fd3ec219c438aa15553ada893beade9b32ee
Vilém Raška <wilhelm.master@gmail.com>
2024-08-29 09:34:08 +0200

    update

--- rust-firmware/src/main.rs
+++ rust-firmware/src/main.rs
@@ -3,51 +3,35 @@
 
 use esp_backtrace as _;
 use esp_hal::{
-    clock::ClockControl, delay::Delay, peripherals::Peripherals, prelude::*, system::SystemControl,
+    clock::ClockControl,
+    delay::Delay,
+    gpio::{Io, Level, Output},
+    peripherals::Peripherals,
+    prelude::*,
+    system::SystemControl,
 };
-
-extern crate alloc;
-use core::mem::MaybeUninit;
-
-#[global_allocator]
-static ALLOCATOR: esp_alloc::EspHeap = esp_alloc::EspHeap::empty();
-
-fn init_heap() {
-    const HEAP_SIZE: usize = 32 * 1024;
-    static mut HEAP: MaybeUninit<[u8; HEAP_SIZE]> = MaybeUninit::uninit();
-
-    unsafe {
-        ALLOCATOR.init(HEAP.as_mut_ptr() as *mut u8, HEAP_SIZE);
-    }
-}
+use esp_println::println;
 
 #[entry]
 fn main() -> ! {
     let peripherals = Peripherals::take();
     let system = SystemControl::new(peripherals.SYSTEM);
+    let clocks = ClockControl::boot_defaults(system.clock_control).freeze();
 
-    let clocks = ClockControl::max(system.clock_control).freeze();
+    println!("Hello world!");
+
+    // Set GPIO7 as an output, and set its state high initially.
+    let io = Io::new(peripherals.GPIO, peripherals.IO_MUX);
+    let mut led = Output::new(io.pins.gpio32, Level::Low);
+
+    led.set_high();
+
+    // Initialize the Delay peripheral, and use it to toggle the LED state in a
+    // loop.
     let delay = Delay::new(&clocks);
-    init_heap();
-
-    esp_println::logger::init_logger_from_env();
-
-    let timer = esp_hal::timer::PeriodicTimer::new(
-        esp_hal::timer::timg::TimerGroup::new(peripherals.TIMG1, &clocks, None)
-            .timer0
-            .into(),
-    );
-    let _init = esp_wifi::initialize(
-        esp_wifi::EspWifiInitFor::Wifi,
-        timer,
-        esp_hal::rng::Rng::new(peripherals.RNG),
-        peripherals.RADIO_CLK,
-        &clocks,
-    )
-    .unwrap();
 
     loop {
-        log::info!("Hello world!");
-        delay.delay(500.millis());
+        led.toggle();
+        delay.delay_millis(500u32);
     }
 }
